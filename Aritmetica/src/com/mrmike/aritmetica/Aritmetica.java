3192d10e8c983b6f770a89139450879a950e09ef
MrMike <MrMike@MrMikeLap>
2019-04-30 08:52:56 -0500

    Metodo para sumar 2 numeros, y su call en metodo main

--- Aritmetica/src/com/mrmike/aritmetica/Aritmetica.java
+++ Aritmetica/src/com/mrmike/aritmetica/Aritmetica.java
@@ -4,6 +4,11 @@ public class Aritmetica {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("resultado: "+ Aritmetica.suma(1, 1));
+	}
+	
+	public static int suma(int a,int b) {
+		return a+b;
 	}
 
 }
